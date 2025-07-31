8a6bd24eda0a4aa93305eea362b46712ce2c6647
Lana <lanazvorygin@gmail.com>
2025-07-30 22:13:00 -0400

    Updated app script link

--- feedback.js
+++ feedback.js
@@ -1,5 +1,5 @@
 function sendFeedback(site, page, feedback) {
-  fetch("https://script.google.com/macros/s/AKfycbypW-SiRt4re080LL4I2Usuy-R_QjRnNZEoLcSb2WEewqpiGiZdHxJQ47X1cmHHlEPPtA/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbwuu0yyjZvKnk8UOS6H3_4J7O-6SvgDjBcrmUZK8bGZEHHoOewMELACFh_JIxWWIoO4Ug/exec", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -17,4 +17,10 @@ function sendFeedback(site, page, feedback) {
   .catch(error => {
     console.error("Error submitting feedback:", error);
   });
+}
+function sendAutoFeedback(feedback) {
+  const site = window.location.hostname.replace(/^www\./, ''); // e.g. "lana-bible-2025.netlify.app"
+  const page = window.location.pathname.split('/').pop() || 'index'; // e.g. "about.html" or "index"
+
+  sendFeedback(site, page, feedback);
 }
